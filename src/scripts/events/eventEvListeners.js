87a78551e27ce13130f9202e5797ba507e0d50c6
Lauren Richert <lsrichert@gmail.com>
2018-07-05 13:41:51 -0500

    working on delete button

--- src/scripts/events/eventEvListeners.js
+++ src/scripts/events/eventEvListeners.js
@@ -1,6 +1,6 @@
 const eventDatabaseHandler = require("./eventDatabaseHandler");
 const eventsPrinter = require("./eventPrinter");
-
+// submit button event listener
 $("#eventForm-container").on("click", "#submit-btn", () => {
     const eventNameInput = $("#eventName-input").val();
     const eventDateInput = $("#eventDate-input").val();
@@ -17,4 +17,15 @@ $("#eventForm-container").on("click", "#submit-btn", () => {
         .then(eventArray => {
             eventsPrinter.printEvents(eventArray)
         })
+})
+// delete button event listener
+$("#event-container").on("click", "delete-btn", () => {
+    const eventID = $(event.target).parent().attr("id")
+    eventDatabaseHandler.deleteEvent(eventID)
+    .then(() => {
+        return eventDatabaseHandler.getAllEvents()
+    })
+    .then((eventArray) => {
+        eventsPrinter.printEvents(eventArray)
+    })
 })
